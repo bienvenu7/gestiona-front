5d1833a5b94574cea0a49309fc59a927303e1fd8
bienvenu7 <ngadzieb@gmail.com>
2026-02-26 18:39:28 +0300

    f

--- configs/socket.config.ts
+++ configs/socket.config.ts
@@ -1,9 +1,10 @@
 import { io, Socket } from "socket.io-client";
 import cookies from "js-cookie";
+import { config } from "./en.config";
 
 const token = cookies.get("accessToken");
 
-export const socket: Socket = io("http://localhost:7001", {
+export const socket: Socket = io(config.api.baseUrl, {
   autoConnect: false,
   withCredentials: true,
   auth: {
